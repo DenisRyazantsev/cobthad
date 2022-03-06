d816dc05f5b29754c530a87b15b41a26b9d621e8
Dennis Ryazantsev <ryazantsev@idrnd.net>
2022-03-06 17:50:44 +0300

    Replace "python3 -m pip download" with "pip install" in download_dependencies.sh

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -4,4 +4,4 @@ echo "$tag Write dependencies to requirements.txt..."
 pipenv lock -r > requirements.txt
 
 echo "$tag Download them into libs folder..."
-python3 -m pip download --destination-directory ./src/cobthad/libs -r requirements.txt --no-binary=:all:
+pip install -r requirements.txt -t "./src/cobthad/libs" --no-binary=:all:
