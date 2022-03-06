7330023e49d8817faba01c0c065f44456dc7f82e
Dennis Ryazantsev <ryazantsev@idrnd.net>
2022-03-06 15:46:30 +0300

    Shrink download_dependencies.sh code

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -1,13 +1,7 @@
 tag="--->"
 
-echo "$tag Update dependencies..."
-pipenv update
-
-echo "$tag Move all dependencies to requirements.py..."
+echo "$tag Write dependencies to requirements.txt..."
 pipenv lock -r > requirements.txt
 
-echo "$tag Move all dependencies to setup.py..."
-pipenv-setup sync
-
 echo "$tag Download them into libs folder..."
-python -m pip download --destination-directory ./src/libs -r requirements.txt --no-binary=:all:
+python3 -m pip download --destination-directory ./src/libs -r requirements.txt --no-binary=:all:
